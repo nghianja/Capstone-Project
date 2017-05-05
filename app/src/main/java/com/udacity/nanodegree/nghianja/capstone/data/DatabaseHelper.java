5659e46a8774755f43d8738c5774410b61a55bb6
James Ng <james.ng@projapps.com>
2017-05-06 00:14:39 +0800

    refactor: remove class and method used for debugging database

--- app/src/main/java/com/udacity/nanodegree/nghianja/capstone/data/DatabaseHelper.java
+++ app/src/main/java/com/udacity/nanodegree/nghianja/capstone/data/DatabaseHelper.java
@@ -1,14 +1,9 @@
 package com.udacity.nanodegree.nghianja.capstone.data;
 
 import android.content.Context;
-import android.database.Cursor;
-import android.database.MatrixCursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
-import java.util.ArrayList;
 
 /**
  * Manages a local database for data.
@@ -71,54 +66,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DataContract.BookEntry.TABLE_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DataContract.LibraryEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
-    }
-
-    /**
-     * To view database for debugging. To be remove along with AndroidDatabaseManager
-     *
-     * References:
-     * [1] https://github.com/sanathp/DatabaseManager_For_Android
-     */
-    public ArrayList<Cursor> getData(String Query){
-        //get writable database
-        SQLiteDatabase sqlDB = this.getWritableDatabase();
-        String[] columns = new String[] { "message" };
-        //an array list of cursor to save two cursors one has results from the query
-        //other cursor stores error message if any errors are triggered
-        ArrayList<Cursor> alc = new ArrayList<Cursor>(2);
-        MatrixCursor Cursor2= new MatrixCursor(columns);
-        alc.add(null);
-        alc.add(null);
-
-        try{
-            String maxQuery = Query ;
-            //execute the query results will be save in Cursor c
-            Cursor c = sqlDB.rawQuery(maxQuery, null);
-
-            //add value to cursor2
-            Cursor2.addRow(new Object[] { "Success" });
-
-            alc.set(1,Cursor2);
-            if (null != c && c.getCount() > 0) {
-                alc.set(0,c);
-                c.moveToFirst();
-                return alc ;
-            }
-            return alc;
-        } catch(SQLException sqlEx){
-            Log.d("printing exception", sqlEx.getMessage());
-            //if any exceptions are triggered save the error message to cursor an return the arraylist
-            Cursor2.addRow(new Object[] { ""+sqlEx.getMessage() });
-            alc.set(1,Cursor2);
-            return alc;
-        } catch(Exception ex){
-
-            Log.d("printing exception", ex.getMessage());
-
-            //if any exceptions are triggered save the error message to cursor an return the arraylist
-            Cursor2.addRow(new Object[] { ""+ex.getMessage() });
-            alc.set(1,Cursor2);
-            return alc;
-        }
     }
 }
